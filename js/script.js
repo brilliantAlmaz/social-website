7ea243c051b5ef1a6910326a1dc6938a436e28f5
Tukhvatulin Almaz <99902545+meowimhere@users.noreply.github.com>
2022-08-06 16:38:22 +0300

    submit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,31 @@
 const headerBurger = document.querySelector('.header__burger');
 const headerMenu = document.querySelector('.header__menu');
-const body = document.querySelector('body');
+const navigation =document.querySelector('.navigation')
+const html = document.querySelector('html');
 headerBurger.addEventListener('click', function(){
 	headerBurger.classList.toggle('active');
 	headerMenu.classList.toggle('active');
-	body.classList.toggle('lock');
+	navigation.classList.toggle('active');
+	html.classList.toggle('lock');
 });
+
+const serviceListItems = document.querySelectorAll('.service__list .service-list-item')
+window.addEventListener('scroll', function(){
+	serviceListItems.forEach(i => {
+		if (onScreen(i)){
+			i.classList.add('animate');
+		}
+	})
+});
+
+
+function onScreen (el) {
+	var rect = el.getBoundingClientRect();
+
+	return (
+		rect.top >= 0 &&
+		rect.left >= 0 &&
+		rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
+		rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+		);
+}
